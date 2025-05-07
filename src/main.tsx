aeb95633bf4e6618bce7c4dd939c1f660fcade48
Zacky Viriot <zackyviriot987@gmail.com>
2025-05-06 19:06:04 -0500

    added browser router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import LoginPage from './Pages/LoginPage/LoginPage.tsx'
+import PatientsPage from './Pages/PatientsPage/PatientsPage.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LoginPage/>} />
+            <Route path = '/patient' element = {<PatientsPage/>}/>
+          </Routes>
+        </BrowserRouter>
+      </React.StrictMode>,
 )
