b1a4927e05dd327ba1feecba415ebabf3c18698c
Zacky Viriot <zackyviriot987@gmail.com>
2025-05-17 18:12:13 -0500

    more error handling

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, } from "react-router-dom";
+import "./services/axiosConfig"; // Import axios configuration
 
 import App from "./App.tsx";
 
